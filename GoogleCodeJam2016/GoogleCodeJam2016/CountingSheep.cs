bc315f4b02db2ec47718120739f8de452f4ecede
Raul Torres Jr <raultorres88@gmail.com>
2016-04-09 11:44:36 -0600

    added packages for first question, set up logging

---
+++ GoogleCodeJam2016/GoogleCodeJam2016/CountingSheep.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace GoogleCodeJam2016.CountingSheep
+{
+    public class CountingSheepCase
+    {
+        public int N;
+        public string Output;
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public void HelloLogger()
+        {
+            logger.Debug("Hello world");
+        }
+    }
+
+  
+
+
+
+
+
+
+
+}
---
+++ GoogleCodeJam2016/GoogleCodeJam2016/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCodeJam2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var x = new GoogleCodeJam2016.CountingSheep.CountingSheepCase();
+            x.HelloLogger();
+        }
+    }
+}
