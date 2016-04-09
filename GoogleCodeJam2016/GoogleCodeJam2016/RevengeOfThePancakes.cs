184a251f9de41692f90f2d12429d08fc6c4f767f
Raul Torres Jr <raultorres88@gmail.com>
2016-04-09 17:02:50 -0600

    tweaking hash

--- GoogleCodeJam2016/GoogleCodeJam2016/RevengeOfThePancakes.cs
+++ GoogleCodeJam2016/GoogleCodeJam2016/RevengeOfThePancakes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 using NUnit.Framework;
 
 namespace GoogleCodeJam2016.RevengeOfThePancakes
@@ -14,15 +15,64 @@ namespace GoogleCodeJam2016.RevengeOfThePancakes
         public string Output;
     }
 
+    public class FlipFinder
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public List<string> Hashes = new List<string>();
+    
+        public PancakeStack FindOptimalStack(IEnumerable<PancakeStack> stacks)
+        {
+            var pancakeStacks = stacks as PancakeStack[] ?? stacks.ToArray();
+            logger.Debug("Evaluating {0} stacks", pancakeStacks.Length);
+
+            // Reach end of recursion if there are no more options to check
+            if (!pancakeStacks.Any())
+                return null;
+
+            var NextStacksToCheck = new List<PancakeStack>();
+
+            // Return any if we are ready
+            foreach (var pancakeStack in pancakeStacks)
+            {
+                if (pancakeStack.ReadyToServe())
+                {
+                    logger.Debug("Found best stack, {0} optimal flips", pancakeStack.Depth);
+                    return pancakeStack;
+                }
+            }
+
+            // Not Good stack, so let's start flipping each stack we have (breadth first)
+            foreach (var pancakeStack in pancakeStacks)
+            {
+                // Keeping track of stacks that have already generated children stacks 
+                Hashes.Add(pancakeStack.GetHash());
+
+                // Flip all possible ways
+                for (var i = 1; i <= pancakeStack.Stack.Length; i++)
+                {
+                    var flippedStack = PancakeFlipper.Flip(i, pancakeStack);
+
+                    // If this is the first time we see the stack, we'll check out all it's options
+                    if(!Hashes.Contains(flippedStack.GetHash()))
+                        NextStacksToCheck.Add(flippedStack);
+                }
+            }
+
+
+            return FindOptimalStack(NextStacksToCheck);
+        }
+
+    }
+
+
     public class PancakeStack
     {
         public int Depth;
         public bool[] Stack;
-        public int Hash;
 
         public PancakeStack(string input)
         {
-            Hash = -1;
+            Depth = 0;      // new stacks start at 0
 
             Stack = new bool[input.Length];
 
@@ -34,13 +84,10 @@ namespace GoogleCodeJam2016.RevengeOfThePancakes
                     Stack[i] = true;
             }
 
-            Hash = GetHash();
-
         }
 
         public PancakeStack(bool[] input)
         {
-            Hash = -1;
             Stack = new bool[input.Length];
 
             for (var i = 0; i < input.Length; i++)
@@ -50,17 +97,14 @@ namespace GoogleCodeJam2016.RevengeOfThePancakes
 
         }
 
-        public int GetHash()
+        public string GetHash()
         {
-            if (Hash > 0)
-                return Hash;
+            return string.Join("", Stack.Select(x => x ? "1" : "0").ToArray()); ;
+        }
 
-            var statckAsString = string.Join("", Stack.Select(x => x ? "1" : "0").ToArray());
-
-            Hash =  Convert.ToInt32(statckAsString, 2);
-
-            return Hash;
-
+        public bool ReadyToServe()
+        {
+            return !Stack.Any(x => x == false);
         }
     }
 
@@ -70,8 +114,8 @@ namespace GoogleCodeJam2016.RevengeOfThePancakes
         {
             var newStack = new PancakeStack(originalStack.Stack);
 
-            // Increade the depth
-            newStack.Depth++;
+            // Increade the depth, or flip count.... thinking about a tree for now, so keeping "depth" verbage
+            newStack.Depth = originalStack.Depth + 1;
 
             // Change the order
             var flipped = newStack.Stack.Take(fromTheTop).ToArray();
@@ -89,47 +133,102 @@ namespace GoogleCodeJam2016.RevengeOfThePancakes
     [TestFixture]
     public class RevengeOfThePancakesTests
     {
-        [TestCase("-", 0 )]
-        [TestCase("+", 1)]
+        [TestCase("-", 1)]
         [TestCase("-+", 1)]
         [TestCase("+-", 2)]
-        [TestCase("+---", 8)]
-        [TestCase("-+++", 7)]
-        public void ConstructorTest(string input, int expectedHash)
+        [TestCase("+++", 0)]
+        [TestCase("--+-", 3)]
+        public void OptimalFlips(string input, int expectedDepth)
+        {
+            var pancakeStack = new PancakeStack(input);
+
+            var flipFinder = new FlipFinder();
+
+            var bestStack = flipFinder.FindOptimalStack(new List<PancakeStack> {pancakeStack});
+
+            Assert.IsNotNull(bestStack);
+            Assert.AreEqual(expectedDepth, bestStack.Depth);
+        }
+
+        //[TestCase(10)]
+        //[TestCase(100)]
+        public void OptimalFlipsBigDataSet(int length)
+        {
+            var pancakeStack = new PancakeStack(new String('-', length));
+
+            var flipFinder = new FlipFinder();
+
+            var bestStack = flipFinder.FindOptimalStack(new List<PancakeStack> { pancakeStack });
+        }
+
+
+        [TestCase("-", "0" )]
+        [TestCase("+", "1")]
+        [TestCase("-+", "01")]
+        [TestCase("+-", "10")]
+        [TestCase("+---", "1000")]
+        [TestCase("-+++", "0111")]
+        public void ConstructorTest(string input, string expectedHash)
         {
             var pancakeStack = new PancakeStack(input);
 
             Assert.AreEqual(expectedHash, pancakeStack.GetHash());
         }
 
-        [TestCase("-", 1)]
-        [TestCase("+", 0)]
-        [TestCase("-+", 2)]
-        [TestCase("+-", 1)]
-        [TestCase("+---", 7)]
-        [TestCase("-+++", 8)]
-        public void FlipTest(string input, int expectedHash)
+        [TestCase("-", "1")]
+        [TestCase("+", "0")]
+        [TestCase("-+", "10")]
+        [TestCase("+-", "01")]
+        [TestCase("+---", "0111")]
+        [TestCase("-+++", "1000")]
+        public void FlipTest(string input, string expectedHash)
         {
             var pancakeStack = new PancakeStack(input);
 
             var newPancakeStack = PancakeFlipper.Flip(pancakeStack.Stack.Length, pancakeStack);
 
             Assert.AreEqual(expectedHash, newPancakeStack.GetHash());
+            Assert.AreEqual(1, newPancakeStack.Depth);
         }
 
-        [TestCase("-", 1)]
-        [TestCase("+", 0)]
-        [TestCase("-+", 3)]
-        [TestCase("+-", 0)]
-        [TestCase("+---", 0)]
-        [TestCase("-+++", 15)]
-        public void FlipTestTopOnly(string input, int expectedHash)
+        [TestCase("-", "1")]
+        [TestCase("+", "0")]
+        [TestCase("-+", "11")]
+        [TestCase("+-", "00")]
+        [TestCase("+---", "0000")]
+        [TestCase("-+++", "1111")]
+        public void FlipTestTopOnly(string input, string expectedHash)
         {
             var pancakeStack = new PancakeStack(input);
 
             var newPancakeStack = PancakeFlipper.Flip(1, pancakeStack);
 
             Assert.AreEqual(expectedHash, newPancakeStack.GetHash());
+            Assert.AreEqual(1, newPancakeStack.Depth);
+        }
+
+        [TestCase("-")]
+        [TestCase("-+")]
+        [TestCase("+-")]
+        [TestCase("+---")]
+        [TestCase("-+++")]
+        public void NotReadyToServe(string input)
+        {
+            var pancakeStack = new PancakeStack(input);
+
+            Assert.IsFalse(pancakeStack.ReadyToServe());
+        }
+
+        [TestCase("+")]
+        [TestCase("++")]
+        [TestCase("+++")]
+        [TestCase("++++")]
+        [TestCase("+++++++")]
+        public void ReadyToServe(string input)
+        {
+            var pancakeStack = new PancakeStack(input);
+
+            Assert.IsTrue(pancakeStack.ReadyToServe());
         }
     }
 }
